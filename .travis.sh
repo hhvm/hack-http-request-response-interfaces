5ff1c7563fb95ac5dba15560e4ad13abba0a77ad
Fred Emmott <fe@fb.com>
2019-02-12 16:09:34 -0800

    test on Travis

--- .travis.sh
+++ .travis.sh
@@ -1,10 +1,21 @@
 #!/bin/sh
 set -ex
+apt update -y
+DEBIAN_FRONTEND=noninteractive apt install -y php-cli zip unzip
 hhvm --version
+php --version
 
-wget https://getcomposer.org/composer.phar
-hhvm ./composer.phar install --ignore-platform-reqs
+(
+  cd $(mktemp -d)
+  curl https://getcomposer.org/installer | php -- --install-dir=/usr/local/bin --filename=composer
+)
+composer install
 
 hh_client
+# no run-time tests for this as it's just interfaces
+# hhvm vendor/bin/hacktest tests/
 
-hhvm vendor/bin/hhast-lint
+# ... but we probably should enable lint
+#if !(hhvm --version | grep -q -- -dev); then
+#  hhvm vendor/bin/hhast-lint
+#fi
